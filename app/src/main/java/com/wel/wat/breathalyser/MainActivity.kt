dd3e62414e1705f1ca854f1a4c6624e4c723d5c9
emil <emil.rybaczewskijlassi@student.wat.edu.pl>
2022-01-19 01:50:35 +0100

    Set radio buttons default check, locked orientation, validation

--- app/src/main/java/com/wel/wat/breathalyser/MainActivity.kt
+++ app/src/main/java/com/wel/wat/breathalyser/MainActivity.kt
@@ -3,12 +3,11 @@ package com.wel.wat.breathalyser
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.view.View
 import android.widget.*
 
 class MainActivity : AppCompatActivity() {
-    var sexValue: Double = 0.0
+    var sexValue: Double = 0.7
     var weightValue: Double = 0.0
     var alcoholValue: Double = 0.0
     var percentageValue: Double = 0.0
@@ -26,15 +25,28 @@ class MainActivity : AppCompatActivity() {
 
         alcochlButton.setOnClickListener {
             val weightEdit: EditText = findViewById(R.id.weight)
-            weightValue = weightEdit.text.toString().toDouble()
-
             val mlEdit: EditText = findViewById(R.id.milliliters)
-            val procentageEditText: EditText = findViewById(R.id.procentage)
-            percentageValue = procentageEditText.text.toString().toDouble()
-            millilitersValue = mlEdit.text.toString().toDouble()
-            alcoholValue = (millilitersValue*(percentageValue/100))*0.8
-            addAlcohol()
-            sober()
+            val percentageEditText: EditText = findViewById(R.id.procentage)
+            when {
+                weightEdit.text.toString().isEmpty() -> {
+                    weightEdit.error = "You need to enter your weight!"
+                }
+                mlEdit.text.toString().isEmpty() -> {
+                    mlEdit.error = "You need to enter amount of alcochol"
+                }
+                percentageEditText.text.toString().isEmpty() -> {
+                    percentageEditText.error = "You need to enter percentages of alcohol"
+                }
+                else -> {
+                    weightValue = weightEdit.text.toString().toDouble()
+                    percentageValue = percentageEditText.text.toString().toDouble()
+                    millilitersValue = mlEdit.text.toString().toDouble()
+                    alcoholValue = (millilitersValue*(percentageValue/100))*0.8
+                    addAlcohol()
+                    sober()
+                }
+            }
+
         }
 
 
@@ -51,7 +63,12 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     private fun sober(){
         val soberText: TextView = findViewById(R.id.sober)
-        unitPerHour = 10/(sexValue*weightValue)
+        if(sexValue==0.6) {
+            unitPerHour = 8 / (sexValue * weightValue)
+        }
+            else {
+            unitPerHour = 10 / (sexValue * weightValue)
+        }
         sober = percentage/unitPerHour
         soberText.text = "You are going to be sober in $sober hours"
 //        val timer = object: CountDownTimer(percentage.toLong(), unitPerHour.toLong()) {
